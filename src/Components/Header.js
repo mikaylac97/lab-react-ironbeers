98c4caad6aa2b33a8b3d877bfa9e08fef589dd99
Mikayla Castro <mikaylacastro97@gmail.com>
2020-10-10 14:30:25 -0400

    router and link setup

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
         <div>
-            <img src='https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png' alt='header' />
+            <Link to='/'>
+                <img src='https://user-images.githubusercontent.com/23629340/40707029-cb2fce12-63ef-11e8-939c-f673ff3b965d.png' alt='header' />
+            </Link>
         </div>
     )
 }
