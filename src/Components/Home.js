78eae133079e03dc9b8eeccf8680d42e3efd7a54
Mikayla Castro <mikaylacastro97@gmail.com>
2020-10-10 14:20:24 -0400

    iteration 1 and 2 done

---
+++ src/Components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+export default function Home() {
+    return (
+        <div className="home">
+            <div>
+                <img src='https://www.tapsbrewery.com/wp-content/uploads/2019/06/TBBR-OurBeer-1024x666.jpg' alt='all beers'/>
+                <h1>All Beers</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in dui at libero efficitur lobortis in sed elit. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed magna tortor, luctus in ultricies non, consequat eu enim. Pellentesque in dapibus sem. Sed condimentum sem dui, vitae mollis augue ullamcorper aliquet. Morbi aliquam urna ut dui lobortis semper. Proin hendrerit scelerisque orci non mattis. Donec faucibus sed nunc eget interdum. Praesent gravida nisi et lobortis euismod. Integer pulvinar lacus non erat ultricies, a fringilla tellus ullamcorper. Quisque eget felis dui. Aenean odio arcu, lobortis et risus quis, vehicula finibus sapien.</p>
+            </div>
+            <div>
+                <img src='https://2fdltvvn8wp2rn64ywgk8o17-wpengine.netdna-ssl.com/wp-content/uploads/2014/04/beer-faucets.jpg' alt='random beer' />
+                <h1>Random Beer</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in dui at libero efficitur lobortis in sed elit. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed magna tortor, luctus in ultricies non, consequat eu enim. Pellentesque in dapibus sem. Sed condimentum sem dui, vitae mollis augue ullamcorper aliquet. Morbi aliquam urna ut dui lobortis semper. Proin hendrerit scelerisque orci non mattis. Donec faucibus sed nunc eget interdum. Praesent gravida nisi et lobortis euismod. Integer pulvinar lacus non erat ultricies, a fringilla tellus ullamcorper. Quisque eget felis dui. Aenean odio arcu, lobortis et risus quis, vehicula finibus sapien.</p>
+            </div>
+            <div>
+                <img src='https://images.creativemarket.com/0.1.0/ps/4136845/910/607/m2/fpnw/wm1/dfcqbakertqqbbjpzbihory58dmyubpampyrqbqhckeybnjd6sfrgst5smnwuqyp-.jpg?1521107814&s=1e6989bd6c7efbff9adf987c92e86128' alt='new beer' />
+                <h1>New Beer</h1> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in dui at libero efficitur lobortis in sed elit. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed magna tortor, luctus in ultricies non, consequat eu enim. Pellentesque in dapibus sem. Sed condimentum sem dui, vitae mollis augue ullamcorper aliquet. Morbi aliquam urna ut dui lobortis semper. Proin hendrerit scelerisque orci non mattis. Donec faucibus sed nunc eget interdum. Praesent gravida nisi et lobortis euismod. Integer pulvinar lacus non erat ultricies, a fringilla tellus ullamcorper. Quisque eget felis dui. Aenean odio arcu, lobortis et risus quis, vehicula finibus sapien.</p>
+            </div>
+        </div>
+    )
+}
