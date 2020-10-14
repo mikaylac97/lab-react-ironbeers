031829376a3f089a0dc429760b95bc0c56efc178
Mikayla Castro <mikaylacastro97@gmail.com>
2020-10-14 18:22:18 -0400

    fixed new beers

--- src/Components/NewBeer.js
+++ src/Components/NewBeer.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import IronBeersAPI from '../api/IronBeersAPI'
+// const axios = require('axios')
 
 export default class NewBeer extends Component {
+    constructor(props) {
+        super(props)
     
-        state = {
-             name: '',
-             tagline: '',
-             description: '',
-             first_brewed: '',
-             brewwers_tips: '',
-             attenuation_level: '',
-             contributed_by: '',
+        this.state = {
+            name: '',
+            tagline: '',
+            description: '',
+            first_brewed: '',
+            brewers_tips: '',
+            attenuation_level: 0,
+            contributed_by: '',
         }
+    }
+    
     
     
     handleChange = (event) => {
+        console.log(event)
         this.setState({
             [event.target.name]: event.target.value
         })
@@ -23,13 +29,9 @@ export default class NewBeer extends Component {
 
     handleSumbit = (event) => {
         event.preventDefault();
-        
         IronBeersAPI
             .createNewBeer(this.state)
-            .then((apiResponse) => {
-            console.log(apiResponse);
-            this.props.history.push('/beers');
-            })
+            .then((apiResponse) => console.log({newBeer: apiResponse.data}))
             .catch((error) => {
             console.log(error);
         });
@@ -43,37 +45,37 @@ export default class NewBeer extends Component {
                     <label>
                         Name:
                         <br/>
-                        <input type='text' name={this.state.name} onChange={this.handleChange}/>
+                        <input type='text' name='name' value={this.state.name} onChange={this.handleChange}/>
                     </label>
                     <label>
                         Tagline:
                         <br/>
-                        <input type='text' name={this.state.tagline} onChange={this.handleChange}/>
+                        <input type='text' name='tagline' value={this.state.tagline} onChange={this.handleChange}/>
                     </label>
                     <label>
                         Description:
                         <br/>
-                        <input type='text' style={{height: '150px'}}name={this.state.description} onChange={this.handleChange}/>
+                        <input type='text' name='description' style={{height: '150px'}}value={this.state.description} onChange={this.handleChange}/>
                     </label>
                     <label>
                         First Brewed:
                         <br/>
-                        <input type='text' name={this.state.first_brewed} onChange={this.handleChange}/>
+                        <input type='text' name='first_brewed' value={this.state.first_brewed} onChange={this.handleChange}/>
                     </label>
                     <label>
                         Brewers Tips:
                         <br/>
-                        <input type='text' name={this.state.brewwers_tips} onChange={this.handleChange}/>
+                        <input type='text' name='brewers_tips' value={this.state.brewers_tips} onChange={this.handleChange}/>
                     </label>
                     <label>
                         Attenuation Level:
                         <br/>
-                        <input type='number' name={this.state.attenuation_level} onChange={this.handleChange}/>
+                        <input type='number' name='attenuation_level' value={this.state.attenuation_level} onChange={this.handleChange}/>
                     </label>
                     <label>
                         Contributed By:
                         <br/>
-                        <input type='text' name={this.state.contributed_by} onChange={this.handleChange}/>
+                        <input type='text' name='contributed_by' value={this.state.contributed_by} onChange={this.handleChange}/>
                     </label>
                     <button>ADD NEW</button>
                 </form>
